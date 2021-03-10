ab843500780f06647c8e12f4a3b2d59c6fd3acb1
marvin-j97 <33938500+marvin-j97@users.noreply.github.com>
2021-03-10 18:45:58 +0100

    Update bundle.ts

--- src/bundle.ts
+++ src/bundle.ts
@@ -2,6 +2,7 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import { rollup } from "rollup";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { uglify } = require("rollup-plugin-uglify");
 
 export async function bundleCode(
